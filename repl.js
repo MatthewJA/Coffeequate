31ed2bf1799d9bc5e2fa62380c70ca698ea3de26
MatthewJA <matthew.alger@gmail.com>
2014-06-24 11:40:21 +1000

    Added REPL.

---
+++ repl.js
@@ -0,0 +1,52 @@
+(function() {
+	var repl = document.getElementById("repl");
+
+	var scrollback = document.createElement("ul");
+	scrollback.className = "scrollback";
+
+	var entryArea = document.createElement("div");
+	entryArea.className = "entry-area";
+	var entry = document.createElement("input");
+	entry.className = "entry prettyprint";
+	entry.setAttribute("tabindex", "1");
+	var entryButton = document.createElement("input");
+	entryButton.type = "submit";
+	entryButton.value = ">";
+	entryButton.className = "entry-button";
+	entryArea.appendChild(entry);
+	entryArea.appendChild(entryButton);
+
+	var style = document.createElement("style");
+	style.innerHTML = ".entry {width: 90%; height: 32px; display: inline-block; font-family: monospace; padding: 0; border: 0; font-size: 28px;}";
+	style.innerHTML += ".entry:focus {background-color: #EFEFFF;}";
+	style.innerHTML += ".entry-area {width: 100%; height: 32px; border: 1px solid #EAEAEA; padding: 0;}";
+	style.innerHTML += ".entry-button {width: 10%; height: 32px; padding: 0; border: 0; margin: 0; vertical-align: top;}";
+	style.innerHTML += ".scrollback {font-family: monospace; list-style-type: none; overflow: auto;}";
+	style.innerHTML += "#repl {width: 500px; margin: 0 auto;}";
+	repl.appendChild(style);
+
+	repl.appendChild(scrollback);
+	repl.appendChild(entryArea);
+
+	function submit() {
+		try {
+			var ret = eval(entry.value);
+		} catch (err) {
+			var ret = err.toString();
+		}
+		var li2 = document.createElement("li");
+		li2.innerHTML = prettyPrintOne(entry.value, "js");
+		scrollback.appendChild(li2);
+		var li = document.createElement("li");
+		li.innerHTML = "&#8594; " + prettyPrintOne(ret, "js");
+		scrollback.appendChild(li);
+		entry.value = "";
+	}
+
+	entry.addEventListener('keypress', function (e) {
+		if (e.keyCode == 13) {
+			submit();
+		}
+	});
+	entryButton.addEventListener('click', submit);
+}).call()
