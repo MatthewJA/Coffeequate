e475a0b357bd3c2e83b65506e8d8b9fe19a712d4
Matthew Alger <matthew.alger@gmail.com>
2015-03-23 18:55:18 +1100

    my good intentions ruined everything

--- coffeequate/compile.sh
+++ coffeequate/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 coffee -c .
-node ./src/vendor/r.js -o ./build/build.js
-node ./src/vendor/r.js -o ./build/build-min.min.js
+node ./src/vendor/r.js -o ./src/build.js
+node ./src/vendor/r.js -o ./src/build-min.min.js
