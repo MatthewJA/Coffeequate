d5c01e976259610a600574891eaf0b3d2c9b4cf5
Matthew Alger <matthew.alger@gmail.com>
2015-03-23 17:41:45 +1100

    wait no I'm wrong

--- coffeequate/compile.sh
+++ coffeequate/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 coffee -c .
-node ./src/vendor/r.js -o ./build/coffeequate.js
-node ./src/vendor/r.js -o ./build/coffeequate.min.js
+node ./src/vendor/r.js -o ./build/build.js
+node ./src/vendor/r.js -o ./build/build-min.min.js
