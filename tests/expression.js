b15f997278699185b4b639ca18e5c6e91662fc5b
MatthewJA <matthew.alger@gmail.com>
2015-06-20 21:10:05 +1000

    Added a few non-trivial tests

--- tests/expression.js
+++ tests/expression.js
@@ -5,10 +5,78 @@ var vows = require('vows'),
 var suite = vows.describe("expression");
 
 suite.addBatch({
-    "An addition expression": {
-        topic: new CQ("a + b + c"),
-        "contains an add node": function(expr) {
-            assert.instanceOf(expr.expr, CQ.raw.Add);
+    "Expression API": {
+        topic: CQ("a + b * c ** d / e - f = g"),
+        "should include toString, toMathML, and toLaTeX": function(expr) {
+            assert.isFunction(expr.toString);
+            assert.isFunction(expr.toMathML);
+            assert.isFunction(expr.toLaTeX);
+        },
+        "should include solve": function(expr) {
+            assert.isFunction(expr.solve);
+        },
+        "should include sub": function(expr) {
+            assert.isFunction(expr.sub);
+        },
+        "should include getAllVariables": function(expr) {
+            assert.isFunction(expr.getAllVariables);
+        },
+        "should include mapOverVariables": function(expr) {
+            assert.isFunction(expr.mapOverVariables);
+        },
+        "should include copy": function(expr) {
+            assert.isFunction(expr.copy);
+        },
+        "should include simplify and expand": function(expr) {
+            assert.isFunction(expr.simplify);
+            assert.isFunction(expr.expand);
+        },
+        "should include differentiate": function(expr) {
+            assert.isFunction(expr.differentiate);
+        },
+        "should include getUncertainty": function(expr) {
+            assert.isFunction(expr.getUncertainty);
+        },
+        "should include toFunction": function(expr) {
+            assert.isFunction(expr.toFunction);
+        },
+        "should include approx": function(expr) {
+            assert.isFunction(expr.approx);
+        },
+        "should include equals": function(expr) {
+            assert.isFunction(expr.equals);
+        }
+    },
+
+    "Expression.solve": {
+        topic: undefined,
+        "can solve trivial equations": function() {
+            var expr = CQ("x = 1");
+            var sol = expr.solve("x");
+            assert.isTrue(CQ("1").equals(sol[0]));
+            assert.lengthOf(sol, 1);
+        },
+        "can solve linear equations": function() {
+            var expr = CQ("y = a*x + b");
+            var sol = expr.solve("x");
+            assert.isTrue(CQ("(y - b)/a").equals(sol[0]));
+            assert.lengthOf(sol, 1);
+            sol = expr.solve("y");
+            assert.isTrue(CQ("a*x + b").equals(sol[0]));
+            assert.lengthOf(sol, 1);
+            sol = expr.solve("b");
+            assert.isTrue(CQ("y - a*x").equals(sol[0]));
+            assert.lengthOf(sol, 1);
+        },
+        "can solve quadratic equations": function() {
+            var expr = CQ("a*x**2 + b*x + c = 0");
+            var sol = expr.solve("x");
+            var pSol = CQ("(-b + (b**2 - 4*a*c)**(1/2))/(2*a)");
+            var nSol = CQ("(-b - (b**2 - 4*a*c)**(1/2))/(2*a)");
+            assert.isTrue(pSol.equals(sol[0]) || pSol.equals(sol[1]));
+            assert.isTrue(nSol.equals(sol[0]) || nSol.equals(sol[1]));
+            assert.isTrue(nSol.equals(sol[0]) || pSol.equals(sol[0]));
+            assert.isTrue(nSol.equals(sol[1]) || pSol.equals(sol[1]));
         }
     }
 }).export(module);
